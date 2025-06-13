ea14e398ff2a6140b665287ff8e0d9af2cb7ec04
William Ngo <williamngowh@gmail.com>
2025-06-14 00:37:04 +1000

    more refactoring on git ignore stuff

---
+++ GarageDoorOpener/app/build/generated/source/buildConfig/debug/com/example/garagedoor/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.garagedoor;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.garagedoor";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final double GEOFENCE_LATITUDE = -37.87696;
+  // Field from build type: debug
+  public static final double GEOFENCE_LONGITUDE = 145.26397;
+  // Field from build type: debug
+  public static final float GEOFENCE_RADIUS = 80f;
+  // Field from build type: debug
+  public static final String httpURL = "https://bwwqsfjkfe.execute-api.us-east-1.amazonaws.com/postTransition";
+}
